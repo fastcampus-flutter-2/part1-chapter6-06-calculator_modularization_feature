34a690e2cb13fc499d27e524e2343ec504ceb694
gasigogi <sh9306@gmail.com>
2024-02-21 01:49:02 +0900

    update

---
+++ starter/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:calculator_modularization_di/di.dart';
+import 'package:calculator_modularization_ui/ui.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(
+    const CalculatorProvider(
+      child: CalculatorApp(),
+    ),
+  );
+}
+
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CalculatorScreen(),
+    );
+  }
+}
